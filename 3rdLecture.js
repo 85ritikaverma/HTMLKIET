0bed8d91959ea7beadb2187735ed01f9e11d3b59
85ritikaverma <142389961+85ritikaverma@users.noreply.github.com>
2024-04-04 20:06:38 +0530

    Add files via upload

---
+++ 3rdLecture.js
@@ -0,0 +1,125 @@
+
+// console.log(message)
+// var message='Hello world';
+// console.log(message)
+// message="else";
+// console.log(message)
+
+// var for declaration of numbers
+// function env(){
+//     for(var i=0;i<4;i++){
+//         console.log(i);
+//     }
+// }
+// env();
+// let for declaration of string
+// constant declaration ke liye sath me initialise karna compulsory h.
+
+let message="hello";
+//  message="else";
+// console.log(message);
+const mess=12;
+// console.log(mess);
+// alert('hello');      
+// alert is use for pop up message
+// we can use (_) underscore alone.
+// let userAge='abcd';
+// let arr=['a', 'c', 'g'];
+// arr[0]='abcd';
+// console.log(arr);
+let str='12';
+let age=20;
+// only plus ke sath concatination ho rha h otherwise it works according to operator.
+let ans=age+str;
+console.log(ans);
+// let b=age-str;
+// let b=age*str;
+// console.log(b);
+// let stri='12a';
+// let p=parseInt(str);
+// let n=Number(str);
+// console.log(p,n);
+
+// let a='120';
+// b=12;
+// if(a==b){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no');
+// }
+// if(a==cpy){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no');
+// }
+
+// const strr='Ritika';
+// const nam="my name is "
+// const anss=nam+strr;
+// console.log(anss);
+// const p= `${nam} ${strr}`;
+// console.log(p);
+// const t=`my name is ${anss}`;
+// // backtic me concatinate ho rha h
+// console.log(t);
+// function fun(str){
+//     console.log(str);
+// }
+// // fun('hello');
+// fun('hello');
+
+// function declaration 
+// function print(){
+//     console.log('--');
+// }
+// function assignment(named)
+// const b=function (){
+// console.log('--');
+// }
+// function assingment (annonymons)
+//prinf() is use for calling function
+// print();
+// b();
+
+// let a=12;
+// switch(a){
+//     case 1:{
+//         console.log('one');
+//     }
+//     case 2:{
+//         console.log('two');
+//     }
+//     default:{
+//         console.log('not ,1,2')
+//     }
+// }
+
+// const obj=new Object();
+// obj[12]='twelve';
+// obj[2]='two';
+// console.log(obj);
+
+// another way of write
+// const obj={};
+// obj[12]='twelve';
+// obj[2]='two';
+// console.log(obj);
+
+// const obj={
+//     1:'one',
+//     'name':'ritika',
+//     11:'eleven',
+// };
+// console.log(obj);
+
+
+
+       const obj={
+        'firstName':'Ajay',
+        'lastName':'singh',
+        'age':20,
+       };
+       const x=prompt("pleae tell");
+       console.log(obj[x]);
